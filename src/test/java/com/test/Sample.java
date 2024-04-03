b76a96b11643d5d753e64a650a10ed0f649ecbfe
rajanovid <abrajan24.7@gmail.com>
2024-04-03 13:20:22 +0530

    first

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class Sample {
+	public void Raj() {
+		
+	
+	int a=10;
+	int b=20;
+	
+	int d=a+b;
+	System.out.println(d);
+	}
+	public static void main(String[] args) {
+		Sample m= new Sample();
+		m.Raj();
+		
+		
+		
+	}
+
+}
